0205fdd0091bf101cbf2bb10b44a544475df5de9
Jafar Esmaili <jaffar9898@gmail.com>
2023-08-04 16:39:39 +0330

    init message consumer

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ from app.api.api import api_router
 app = FastAPI(title="FastAPI Elastic Example Project")
 
 app.include_router(api_router, prefix="/api")
+from app.core.listener import init_listen_queues
 
 
 @app.get("/")
@@ -16,6 +17,8 @@ async def root():
 async def startup():
     if not database.is_connected:
         await database.connect()
+    # consume_message()
+    init_listen_queues()
 
 @app.on_event("shutdown")
 async def shutdown():
